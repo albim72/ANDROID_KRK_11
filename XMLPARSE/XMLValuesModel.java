993328fb2074538d901eb4f9051f647ceaa8bab4
albim72 <marcin.albiniak72@gmail.com>
2023-11-22 11:58:26 +0100

    Create XMLValuesModel.java

---
+++ XMLPARSE/XMLValuesModel.java
@@ -0,0 +1,103 @@
+package com.marcin.xmlparser;
+
+public class XMLValuesModel {
+    private int id;
+    private int company_id;
+    private String company = "";
+    private String date = "";
+    private String address = "";
+    private String city = "";
+    private String state = "";
+    private String zipcode = "";
+    private String country = "";
+    private String telephone = "";
+    private String forms = "";
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCompany_id() {
+        return company_id;
+    }
+
+    public void setCompany_id(int company_id) {
+        this.company_id = company_id;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getForms() {
+        return forms;
+    }
+
+    public void setForms(String forms) {
+        this.forms = forms;
+    }
+}
