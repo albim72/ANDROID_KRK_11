0c688f94a86fec234f10209e4c4726683bafbed2
albim72 <marcin.albiniak72@gmail.com>
2023-11-22 09:56:30 +0100

    Create IPrzeliczniki.java

---
+++ KALKULATORODLEGLOSCI/IPrzeliczniki.java
@@ -0,0 +1,10 @@
+package com.marcin.kalkulatorodleglosci;
+
+public interface IPrzeliczniki {
+    double milaPP = 1852.0;
+    double milaLP = 1622.0;
+    double jardP = 0.9144;
+    double stopaP = 0.30477;
+    double calP = 0.0254;
+}
+
